ffe2ac64f6f4fd189ae47942d5671920c3f30233
Tom Craig <tcraig@pandora.com>
2018-08-11 00:33:52 -0700

    Update readme and cleanup karabiner

---
+++ ergodox_keymaps/tom-setup.sh
@@ -0,0 +1,8 @@
+#Install pyenv, python 3.3.6
+# brew cmake ctags
+# brew install libusb-1.0-0-dev
+# brew install binutils-arm-none-eabi
+# brew install gcc-arm-none-eabi
+# brew install libnewlib-arm-none-eabi dfu-util
+#Setup pipenv
+	#pipenv install --python ~/.pyenv/versions/3.3.6/bin/python
