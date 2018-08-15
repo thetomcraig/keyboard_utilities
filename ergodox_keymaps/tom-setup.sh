867f02d174465b7ad1a4b2293844825ca6533272
Tom Craig <tcraig@pandora.com>
2018-08-15 16:12:40 -0700

    Better Instructions for ergodox python setup

--- ergodox_keymaps/tom-setup.sh
+++ ergodox_keymaps/tom-setup.sh
@@ -5,4 +5,8 @@
 # brew install gcc-arm-none-eabi
 # brew install libnewlib-arm-none-eabi dfu-util
 #Setup pipenv
-	#pipenv install --python ~/.pyenv/versions/3.3.6/bin/python
+#cd controller-repo/Keyboards 
+#pipenv install 3.7.0
+#pipenv install --python ~/.pyenv/versions/3.7.0/bin/python
+#pipenv shell
+#./ergodox.bash
