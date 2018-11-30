7054356e22fbbce99401a0e1a9aedccad5b747f3
Tom Craig <thetomcraig@icloud.com>
2018-11-30 15:31:12 -0800

    Remove make files

--- ergodox/controller/Keyboards/darwin18.0.0.IC60.gcc.make/CMakeFiles/kll_display.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kll_display"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kll_display.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
