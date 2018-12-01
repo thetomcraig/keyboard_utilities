a1ebf3d227a9062ef89415f2c14df55dcd08c948
Tom Craig <thetomcraig@icloud.com>
2018-11-30 17:27:11 -0800

    Remove old kll source

--- ergodox/controller/Keyboards/darwin18.0.0.ICED-L.gcc.make/CMakeFiles/SizeAfter.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SizeAfter"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SizeAfter.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
