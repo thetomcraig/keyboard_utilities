7aee77d482286f3763b9fa8cf613ef2126b87c49
Tom Craig <thetomcraig@icloud.com>
2018-12-03 21:11:37 -0800

    Remove controller repo

--- ergodox/controller/Keyboards/darwin18.0.0.ICED-R.gcc.make/CMakeFiles/kll_debug.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kll_debug"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kll_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ergodox/controller/Keyboards/darwin18.0.0.ICED-R.gcc.make/CMakeFiles/kll_token.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kll_token"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kll_token.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
