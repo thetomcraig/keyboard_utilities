63ffc10f6a7318325f5ca64c33462b621104c204
Tom Craig <thetomcraig@icloud.com>
2018-12-11 17:02:25 -0800

    Remove old junk; finish script

--- ergodox/flash_new_bin.sh
+++ ergodox/flash_new_bin.sh
@@ -1,8 +1,15 @@
 # Generate the new binary (configurator, whatever)
 # Place binary here and rename it "ergodox_binary.zip"
 
-unzip ergodox_binary.zip ergodox_binary_files
+rm -rf ergodox_binary_files
+mkdir ergodox_binary_files
+
+unzip ergodox_binary.zip -d ergodox_binary_files
+
 cd ergodox_binary_files
 echo "Put dox in flash mode, then wait 5s"
 sleep 5
-dfu dfu-util -D right_kiibohd.dfu.bin
+dfu-util -D right_kiibohd.dfu.bin
+
+cd ..
+rm -rf ergodox_binary_files
