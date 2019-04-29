2e54a4a3fc80ceef1944283495e392e4c8537cb1
Tom Craig <thetomcraig@icloud.com>
2019-04-29 11:52:47 -0700

    Remove old shit

--- ergodox_infinity_maps/flash_new_bin.sh
+++ ergodox_infinity_maps/flash_new_bin.sh
@@ -1,14 +1,4 @@
 # Generate the new binary (configurator, whatever)
-
-rm -rf ergodox_binary_files
-mkdir ergodox_binary_files
-
-unzip "${1}" -d ergodox_binary_files
-
-cd ergodox_binary_files
 echo "Put dox in flash mode, then wait 5s"
 sleep 5
-dfu-util -D right_kiibohd.dfu.bin
-
-cd ..
-rm -rf ergodox_binary_files
+dfu-util -D "${1}" 
