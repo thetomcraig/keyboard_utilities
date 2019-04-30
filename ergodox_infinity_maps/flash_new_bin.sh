82b170bddb4f073436177ed3e7c85a344de765c4
Tom Craig <thetomcraig@icloud.com>
2019-04-30 12:58:39 -0700

    Updated the flash instructions

--- ergodox_infinity_maps/flash_new_bin.sh
+++ ergodox_infinity_maps/flash_new_bin.sh
@@ -1,4 +1,23 @@
-# Generate the new binary (configurator, whatever)
-echo "Put dox in flash mode, then wait 5s"
+# Setup left
+echo "Get sudo password saved"
+sudo ls
+echo "Make left side"
+make ergodox_infinity:${1}
+
+# Flash left!
+echo "Unplug, then plug in ONLY left"
 sleep 5
-dfu-util -D "${1}" 
+echo "Put left side into flash mode"
+sleep 2
+sudo make ergodox_infinity:${1}:dfu-util
+
+# Setup left
+echo "Make right side"
+make ergodox_infinity:${1} MASTER=right
+
+# Flash right!
+echo "Unplug, then plug in ONLY right"
+sleep 5
+echo "Put right side into flash mode"
+sleep 2
+sudo make ergodox_infinity:${1}:dfu-util MASTER=right
