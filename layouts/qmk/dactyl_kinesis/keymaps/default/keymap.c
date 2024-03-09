f309542f14e95c21d98738c66177900ac0e1df1e
Tom Craig <thetomcraig@icloud.com>
2024-03-09 12:47:42 -0800

    wip

--- layouts/qmk/dactyl_kinesis/keymaps/default/keymap.c
+++ layouts/qmk/dactyl_kinesis/keymaps/default/keymap.c
@@ -18,8 +18,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
                  KC_GRV , KC_BSLS, KC_LEFT, KC_RGHT,                                            KC_UP  , KC_DOWN, KC_LBRC, KC_RBRC,
-                                                     KC_LGUI, KC_LALT,        KC_RCTL, KC_RGUI,
-                                            KC_BSPC,  KC_DEL, KC_HOME,        KC_PGUP, KC_ENT , KC_SPC ,
-                                                              KC_END ,        KC_PGDN
+                                                             KC_MS_L, KC_BTN1, KC_MS_R,
+                                   KC_LGUI, KC_LALT,                                           KC_RCTL, KC_RGUI,
+                          KC_BSPC,  KC_DEL, KC_HOME,                                           KC_PGUP, KC_ENT , KC_SPC ,
+                                            KC_END ,                                           KC_PGDN
     )
 };
