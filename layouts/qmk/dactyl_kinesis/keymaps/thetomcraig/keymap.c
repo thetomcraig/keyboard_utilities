9fba8b9c8dce952357a4efc7d19a2e9b78fd5281
Tom Craig <thetomcraig@icloud.com>
2024-07-06 15:53:57 -0700

    mission control

--- layouts/qmk/dactyl_kinesis/keymaps/thetomcraig/keymap.c
+++ layouts/qmk/dactyl_kinesis/keymaps/thetomcraig/keymap.c
@@ -161,7 +161,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC , KC_F1 , KC_F2       , KC_F3       , KC_F4       , KC_F5  ,          KC_F6  , KC_F7       , KC_F8          , KC_F9       , KC_F10 , KC_F11 ,
-        KC_EQL , KC_1  , KC_2        , KC_3        , KC_4        , KC_5   ,          KC_6   , KC_7        , KC_8           , KC_9        , KC_0   , KC_MINS,
+        KC_EQL , KC_1  , KC_2        , KC_3        , KC_4        , KC_MCTL,          KC_MCTL, KC_7        , KC_8           , KC_9        , KC_0   , KC_MINS,
         KC_TAB , KC_Q  , KC_W        , KC_E        , KC_R        , KC_T   ,          KC_Y   , KC_U        , KC_I           , KC_O        , KC_P   , KC_BSLS,
         KC_LCTL, KC_A  , LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G   ,          KC_H   , RSFT_T(KC_J), RGUI_T(KC_K)   , RCTL_T(KC_L), KC_QUOT, KC_SCLN,
         KC_LSFT, KC_Z  , KC_X        , KC_C        , LALT_T(KC_V), KC_B   ,          KC_N   , RALT_T(KC_M), KC_COMM        , KC_DOT      , KC_SLSH, KC_RSFT,
